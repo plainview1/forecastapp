36dd4eb449077993ce61050ac434c675de815638
plainview1 <illia.panchenko@senseer.ai>
2023-08-18 02:44:09 +0300

    added caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { forkJoin, Observable, of, throwError } from 'rxjs';
-import { distinctUntilChanged, debounceTime, filter, catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { distinctUntilChanged, debounceTime, filter, catchError, map, shareReplay } from 'rxjs/operators';
 
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
@@ -59,17 +59,19 @@ export class AppComponent implements OnInit {
 export class ForecastService {
   private apiKey = `2d42a22d65658c13b1bf85a24c188eda`;
   private apiLink = `https://api.openweathermap.org/data/2.5`;
+  private cache: { [city: string]: { timestamp: number; data: Observable<any> } } = {};
 
   constructor(private http: HttpClient) {
   }
 
   searchCitiesByName(cityName: string): Observable<any> {
     const url = `${this.apiLink}/find?q=${cityName}&appid=${this.apiKey}`;
-    return this.http.get(url).pipe(catchError(this.handleError));
+    return this.checkInCacheOrRequest(url, `list_${cityName}`);
   }
 
   getCurrentWeather(cityName: string): Observable<any> {
-    return this.http.get(`${this.apiLink}/weather?q=${cityName}&appid=${this.apiKey}`).pipe(map((item: any) => {
+    const url = `${this.apiLink}/weather?q=${cityName}&appid=${this.apiKey}`;
+    return this.checkInCacheOrRequest(url, `current_${cityName}`).pipe(map((item: any) => {
       item.main.temp = Math.round(item.main.temp);
       return item;
     }))
@@ -77,7 +79,37 @@ export class ForecastService {
 
   getForecastedWeather(cityName: string): Observable<any> {
     const url = `${this.apiLink}/forecast?q=${cityName}&cnt=5&appid=${this.apiKey}`
-    return this.http.get(url).pipe(catchError(this.handleError));
+    return this.checkInCacheOrRequest(url, `forecast_${cityName}`);
+  }
+
+  private checkInCacheOrRequest(url: string, cityName: string): Observable<any> {
+    const cachedData = this.cache[cityName];
+    if (cachedData && this.isDataFresh(cachedData.timestamp)) {
+      return cachedData.data;
+    } else {
+      const freshData = this.getFreshData(url);
+      this.cache[cityName] = { timestamp: Date.now(), data: freshData };
+      return freshData;
+    }
+  }
+
+  private isDataFresh(timestamp: number): boolean {
+    const currentTime = Date.now();
+    const timeDifference = currentTime - timestamp;
+    const oneHourInMilliseconds = 60 * 60 * 1000;
+    return timeDifference < oneHourInMilliseconds;
+  }
+
+  private getFreshData(url: string): Observable<any> {
+    return this.fetchData(url).pipe(
+      shareReplay(1)
+    );
+  }
+
+  private fetchData(url: string): Observable<any> {
+    return this.http.get(url).pipe(
+      catchError(this.handleError)
+    );
   }
 
   private handleError(error: HttpErrorResponse): Observable<never> {
